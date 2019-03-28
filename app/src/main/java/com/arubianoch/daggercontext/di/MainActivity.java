69ebd9684224531006f89a801e3cf2c762854740
Andres Rubiano Del Chiaro <andres.rubiano.ch@gmail.com>
2019-03-28 18:27:28 -0500

    Added Field Injection to test if injection has been working as expected

--- app/src/main/java/com/arubianoch/daggercontext/di/MainActivity.java
+++ app/src/main/java/com/arubianoch/daggercontext/di/MainActivity.java
@@ -1,13 +1,29 @@
-package com.arubianoch.daggercontext;
+package com.arubianoch.daggercontext.di;
 
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.arubianoch.daggercontext.R;
+
+import javax.inject.Inject;
+
 public class MainActivity extends AppCompatActivity {
+
+    @Inject
+    SharedPreferences preferences;
+
+    @Inject
+    AppName appName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ((MyApplication) getApplicationContext()).getMyComponent().inject(this);
+
+        preferences.edit().putInt("Num2", 4).apply();
+        preferences.edit().putString("AppName", appName.getApp()).apply();
     }
 }
